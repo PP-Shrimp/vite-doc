b127f4ffab51215d24dbf303412fd39cc95d28ac
hjl <hjl@sagacloud.com>
2021-06-25 16:35:31 +0800

    重新打包

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ const configSide = require('../config/index')
 const testSide = require('../vuecomtest/index')
 
 module.exports = {
-    base: '/vite-doc',
+    base: '/vite-doc/dist',
     lang: 'en-US',
     title: 'VitePress',
     description: 'Vite & Vue powered static site generator.',
